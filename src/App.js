1f8064a0649681ee02794cdaef44f06082d12748
Charlie Guan <charliegdev@gmail.com>
2018-09-22 17:32:57 -0700

    Add a topbar with a search field.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import SearchAppBar from "./components/SearchAppBar";
 import CenteredGrid from "./components/CenteredGrid";
 
 
 class App extends Component {
   render() {
     return (
-      <CenteredGrid />
+      <Fragment>
+        <SearchAppBar />
+        <CenteredGrid />
+      </Fragment>
     );
   }
 }
